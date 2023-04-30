ec863ef47044d1c35f4b58ab0c6bdcb25e314fd0
marioCoding <marosw.sal@gmail.com>
2023-04-29 17:26:41 -0700

    final push

---
+++ js/climateAPI.js
@@ -0,0 +1,25 @@
+const section2 = document.querySelector("#section2-wrapper");
+const subSection2 = document.getElementById("section4");
+const distanceResults = document.getElementById("distance-results");
+const vehicleResults = document.getElementById("vehicle-results");
+
+function submitButton() {
+  const distance = document.getElementById("distance");
+  const vehicle = document.getElementById("vehicle");  
+  const request = {
+    distance: distance.value,
+    vehicle: vehicle.value
+  }
+  //where you do the fetch reqeust
+  //data == respom
+  const data_from_fetch = {
+    co2e: 1.54
+  }
+  console.log(request);
+  
+  // hide .section2
+  section2.style.display = "none";
+  subSection2.style.display = "flex";
+  distanceResults.innerText = `Distance traveled: ${distance.value}`;
+  vehicleResults.innerText = `Vehicle type: ${vehicle.value}`;
+}   
